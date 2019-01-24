93ba9d15d4699b271397f544d4d440aa0318e3f9
itome <itometeam@gmail.com>
2019-01-24 13:47:06 +0900

    Add provide

--- starwars.el
+++ starwars.el
@@ -1,3 +1,34 @@
+;;; starwars.el --- Starwars on Emacs
+
+;; Copyright (C) 2017  takeshi tsukamoto
+
+;; Author: Takeshi Tsukamoto <itometeam@gmail.com>
+;; URL: https://github.com/itome/emacs-starwars
+;; Created: 20190123
+;; Version: 0.1.0
+;; Status: experimental
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;; Starwars on Emacs
+;; Just run M-x starwars
+
+;;; Code:
 (defun starwars ()
   (interactive)
   (switch-to-buffer (get-buffer-create "starwars"))
@@ -21,3 +52,5 @@
   (with-temp-buffer
     (insert-file-contents filePath)
     (split-string (buffer-string) "\n" nil nil)))
+
+(provide 'starwars)
